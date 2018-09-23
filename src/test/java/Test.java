7224eba5d8824cef0c67e370c7bcb446cab88739
fxy <2974187202@qq.com>
2018-09-23 21:39:55 +0800

    utils更新

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,7 +2,22 @@ public class Test {
     @org.junit.Test
     public void test() {
         Utils utils = new Utils();
-        String[] s = {"-","*"};
+        String[] s = {"+","-","*"};
         utils.fourNum(s);
+    }
+
+    @org.junit.Test
+    public void test1(){
+        int num0 = 0;
+        int num12 = 0;
+        for (int i=0;i<100;i++) {
+            if((int)(Math.random()*3) == 0) {
+                num0++;
+                continue;
+            }
+            num12++;
+        }
+        System.out.println("num0:"+num0);
+        System.out.println("num12:"+num12);
     }
 }
